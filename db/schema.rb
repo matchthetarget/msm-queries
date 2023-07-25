385466285cf19ad012356bbcb1905862312f8ae3
Ben Purinton <ben.purinton@gmail.com>
2023-07-25 11:26:23 -0800

    ad1 update

--- db/schema.rb
+++ db/schema.rb
@@ -10,88 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_205051) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "actors", force: :cascade do |t|
-    t.string "name"
-    t.date "dob"
-    t.text "bio"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "characters", force: :cascade do |t|
-    t.integer "movie_id"
-    t.integer "actor_id"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "photo_id"
-    t.text "body"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "directors", force: :cascade do |t|
-    t.string "name"
-    t.date "dob"
-    t.text "bio"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "follow_requests", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "recipient_id"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "fan_id"
-    t.integer "photo_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "year"
-    t.integer "duration"
-    t.text "description"
-    t.string "image"
-    t.integer "director_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.text "caption"
-    t.string "image"
-    t.integer "owner_id"
-    t.integer "likes_count"
-    t.integer "comments_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.boolean "private"
-    t.integer "likes_count"
-    t.integer "comments_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-  end
 
 end
